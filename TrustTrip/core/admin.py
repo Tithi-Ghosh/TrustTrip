052b689a75368021262e4a2dc55695395cdce770
RadiaLia <23101060@uap-bd.edu>
2025-10-24 00:56:03 +0600

    Update admin.py

--- TrustTrip/core/admin.py
+++ TrustTrip/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here.from django.contrib import admin
+from .models import BookingDriverSide, BookingUserSide, Payment, Review
+
+admin.site.register(BookingDriverSide)
+admin.site.register(BookingUserSide)
+admin.site.register(Payment)
+admin.site.register(Review)
+
